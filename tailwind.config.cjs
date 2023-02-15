2a1c5da36402e6068e95510f5e27357434bcff31
runimo94 <runimo94git@gmail.com>
2023-02-15 22:23:00 +0100

    add assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        myBackground: "url(bg-blurry.svg)",
+      },
+      fontFamily: {
+        Rubik: ["Rubik", "sans-serif"],
+        LexendDeca: ["Lexend Deca", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
