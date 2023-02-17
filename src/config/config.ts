1dab0e2281b5230c2469571db7002a3e01d13e72
runimo94 <runimo94git@gmail.com>
2023-02-17 13:38:03 +0100

    archivo de datos

---
+++ src/config/config.ts
@@ -0,0 +1,73 @@
+export const dataHeader = {
+  listSections: [
+    {
+      text: "Inicio",
+      id: "#Inicio",
+    },
+    {
+      text: "Sobre mi",
+      id: "#SobreMi",
+    },
+    {
+      text: "Proyectos",
+      id: "#Proyectos",
+    },
+    {
+      text: "Habilidades",
+      id: "#Habilidades",
+    },
+    {
+      text: "Contacto",
+      id: "#Contacto",
+    },
+  ],
+};
+
+export const dataInicio = {
+  textHeader: "Hola, soy Rubén Nicolas",
+  textHeader2: "<Desarrollador fullstack/>",
+  listButtons: [
+    {
+      text: "Descarga CV",
+      url: "",
+    },
+    {
+      text: "GitHub",
+      url: "https://github.com/runimo94",
+    },
+    {
+      text: "LinkedIn",
+      url: "",
+    },
+  ],
+};
+
+export const dataAboutMe = {
+  image: "runimo94.png",
+  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, vitae voluptatibus accusamus cum perferendis corporis soluta inventore repellendus, quam quia, dolores autem repudiandae sunt. Quia delectus, provident quo corporis dolorum quaerat rem illo reprehenderit earum vero magni eum distinctio, quibusdam quas quasi quod vitae minima id ullam aspernatur esse veritatis?",
+  subtext:
+    "*Esta no es una imagen real. Es una imagen generada con Stable Diffusion, una IA generativa de imagenes. Usando un modelo entrenado por mi, con imagenes propias.",
+};
+
+export const dataProjects = {
+  listProjects: [
+    {
+      image: "project1.png",
+      title: "MonsterCat Release Album Page Clone",
+      urlCode: "https://github.com/runimo94/album-release-page",
+      url: "https://runimo94.github.io/album-release-page/",
+    },
+    {
+      image: "project2.png",
+      title: "World of Warcraft Landing Page Expansion Clone",
+      urlCode: "https://github.com/runimo94/landing-page-warcraft",
+      url: "https://runimo94.github.io/landing-page-warcraft/",
+    },
+    {
+      image: "project1.png",
+      title: "Portfolio personal",
+      urlCode: "https://github.com/runimo94/landing-page-warcraft",
+      url: "https://runimo94.github.io/landing-page-warcraft/",
+    },
+  ],
+};
