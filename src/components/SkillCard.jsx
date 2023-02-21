c6c2e172cf4b2e4b4c391d7cefcd2db087797cd0
runimo94 <runimo94git@gmail.com>
2023-02-21 23:38:01 +0100

    Animacion dropdown del content de las cards

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,4 +1,5 @@
 import IconTech from "./IconTech.jsx";
+import "./SkillCard.css";
 
 export const SkillCardComponent = ({
   skill,
@@ -7,49 +8,55 @@ export const SkillCardComponent = ({
   showInfo,
 }) => {
   return (
-    <div
-      id={skill.id}
-      className={
-        (active ? "flex" : "hidden") +
-        " skill-card flex-col justify-between gap-y-10 bg-slate-800 p-7 lg:p-10 rounded-3xl"
-      }
-    >
-      <div className="flex flex-col gap-y-10">
-        <div className="flex flex-row justify-between">
-          <h1 className="text-white text-base sm:text-xl lg:text-4xl font-LexendDeca text-start">
-            {skill.skillName}
-          </h1>
-          <button
-            onClick={() => buttonClick(skill.id)}
-            className="inline lg:hidden text-white"
-          >
-            <img
-              className={(showInfo ? "-rotate-90" : "") + " w-6"}
-              src="icons/arrow_down.svg"
-              alt=""
-            />
-          </button>
-        </div>
-        <p
-          className={
-            (showInfo ? "inline" : "hidden") +
-            " font-Rubik text-white text-justify mx-auto text-base lg:text-start lg:mx-0 xl:text-xl"
-          }
-        >
-          {skill.text}
-        </p>
-      </div>
+    <div className={(active ? "flex" : "hidden") + " flex flex-col"}>
       <div
         className={
-          (showInfo ? "flex" : "hidden") +
-          " items-center justify-center gap-x-6"
+          (showInfo ? "rounded-t-3xl" : "rounded-3xl") +
+          " flex justify-between bg-slate-800 px-7 lg:px-10 py-5 cursor-pointer"
+        }
+        onClick={() => buttonClick(skill.id)}
+      >
+        <h1 className="text-white text-base sm:text-xl lg:text-3xl font-LexendDeca font-bold text-start">
+          {skill.skillName}
+        </h1>
+        <img
+          className={(showInfo ? "-rotate-90" : "") + " inline lg:hidden w-6"}
+          src="icons/arrow_down.svg"
+          alt=""
+        />
+      </div>
+
+      <div
+        className={
+          (showInfo ? "expand" : "") +
+          " expandable-text  bg-slate-800 rounded-b-3xl "
         }
       >
-        {skill.techIcons.map((tech, index) => {
-          return (
-            <IconTech key={index} classes="w-7 md:w-10 lg:w-16" tech={tech} />
-          );
-        })}
+        <div
+          className={
+            (showInfo ? "block" : "hidden") +
+            " flex flex-col gap-y-10 px-7 lg:px-10"
+          }
+        >
+          <p
+            className={
+              " font-Rubik text-white text-justify mx-auto text-base lg:text-start lg:mx-0 xl:text-xl pt-5"
+            }
+          >
+            {skill.text}
+          </p>
+          <div className={"flex items-center justify-center gap-x-6 pb-10"}>
+            {skill.techIcons.map((tech, index) => {
+              return (
+                <IconTech
+                  key={index}
+                  classes="w-7 md:w-10 lg:w-16"
+                  tech={tech}
+                />
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
