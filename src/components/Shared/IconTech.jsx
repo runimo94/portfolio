bbf5403887a00dd3a1f65444c610123dba9a443d
runimo94 <runimo94git@gmail.com>
2023-02-25 17:30:31 +0100

    agregados assets

--- src/components/Shared/IconTech.jsx
+++ src/components/Shared/IconTech.jsx
@@ -3,12 +3,25 @@ import "./IconTech.css";
 
 const IconDict = {
   angular: "icons/angular.svg",
+  android: "icons/android.svg",
+  "android studio": "icons/androidstudio.svg",
   astro: "icons/astro.svg",
+  express: "icons/express.svg",
+  dotnet: "icons/dotnet.svg",
+  mongodb: "icons/mongodb.svg",
+  "sql server": "icons/sqlserver.svg",
   tailwind: "icons/tailwindcss.svg",
   javascript: "icons/javascript.svg",
   css: "icons/css3.svg",
   html: "icons/html5.svg",
   react: "icons/react.svg",
+  nodejs: "icons/nodejs.svg",
+  xamarin: "icons/xamarin.svg",
+  xaml: "icons/xaml.svg",
+  "visual Studio": "icons/visualstudio.svg",
+  "vs code": "icons/visualstudiocode.svg",
+  git: "icons/git.svg",
+  nuget: "icons/nuget.svg",
 };
 
 export const IconTechComponent = ({ tech, classes }) => {
